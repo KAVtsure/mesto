ea544b54712d3a0fabca8d6dbdb47dee95bd59f6
Andrey Kuzmenko <kav.tagan@yandex.ru>
2022-04-15 00:04:47 +0300

    project 4 fix after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,38 @@
 let formElement = document.querySelector('.popup__edit');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__description');
+let nameInput = formElement.querySelector('.popup__field_name');
+let jobInput = formElement.querySelector('.popup__field_description');
+
+
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-
 // Обработчик «отправки» формы
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
+    toggleModalWindow();
 }
-
 // Обработчик следит за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
-// Открытие-закрытие окна редактирования профиля пользователя
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const ModalWindow = document.querySelector('.popup');
-const ModalWindowCloseBtn = ModalWindow.querySelector('.popup__close-button');
 
+// Открытие-закрытие окна редактирования профиля пользователя
+const ModalWindow = document.querySelector('.popup');
 function toggleModalWindow() {
     ModalWindow.classList.toggle('popup_opened');
 }
 
-// Обработчик следит за событием “click” - «Открытие/закрытие окна редактирования профиля пользователя по кнопке»
-profileEditBtn.addEventListener('click', toggleModalWindow);
+const ModalWindowCloseBtn = ModalWindow.querySelector('.popup__close-button');
+// Обработчик следит за событием “click” - «закрытие окна редактирования профиля пользователя по кнопке "Закрыть"»
 ModalWindowCloseBtn.addEventListener('click', toggleModalWindow);
 
 
-formElement.addEventListener('submit', toggleModalWindow);// Обработчик следит за событием “submit” - "закрытие модального окна"
-
-
+const profileEditBtn = document.querySelector('.profile__edit-button');
 // Вносит в поля формы текущие значения профиля пользователя
 function openProfileEdit() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
+    toggleModalWindow();
 }
-
-profileEditBtn.addEventListener('click', openProfileEdit);// Обработчик следит за событием “click” - «Внесение текущих значений профиля пользователя в поля формы»
+// Обработчик следит за событием “click” - «Внесение текущих значений профиля пользователя в поля формы по кнопке "Редактировать профиль"»
+profileEditBtn.addEventListener('click', openProfileEdit);
